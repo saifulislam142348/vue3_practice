b3c37b48d22a052f05d316eb57a470e0ab5ce2c5
saifulislam142348 <saiful7182103353@gmail.com>
2023-05-19 20:29:28 +0600

    cmponet add

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ContactView name="kamal"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ContactView from './components/ContactView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ContactView
   }
 }
 </script>
---
+++ src/components/ContactView.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>My adress </h1>
+<p>name:{{ name }}</p>
+<p>village:Dhaka</p>
+<p>contact:0162086756</p>
+</template>
+
+<script>
+export default {
+  props: ['name']
+}
+</script>
+
+<style>
+
+</style>

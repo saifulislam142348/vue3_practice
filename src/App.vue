20bd0ad44a405ebd50060d05a442816064e648d6
saifulislam142348 <saiful7182103353@gmail.com>
2023-05-24 14:19:16 +0600

    slot

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,28 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   <ContactView name="kamal"/>
+  <TheCard name="saiful islam" >
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque odio voluptatum animi natus soluta alias laudantium pariatur sit est accusantium ipsam, eum nostrum? Distinctio blanditiis sint ab impedit quas fugit quam dolor culpa labore excepturi dolorum reprehenderit voluptate placeat expedita asperiores sunt in laborum facere, sit veritatis suscipit qui tempore?</p>
+  </TheCard>
+   <TheCard name="title nme" >
+    <template v-slot:info>
+   <p>laudantium pariatur sit est accusantium ipsam, eum nostrum? Distinctio blanditiis sint ab impedit quas fugit quam dolor culpa labore excepturi dolorum reprehenderit voluptate placeat expedita asperiores sunt in laborum facere, sit veritatis suscipit qui tempore?</p>
+    </template>
+       
+    </TheCard>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import ContactView from './components/ContactView.vue'
+import TheCard from './components/TheCard.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    ContactView
+    ContactView,
+    TheCard
   }
 }
 </script>
---
+++ src/components/TheCard.vue
@@ -0,0 +1,43 @@
+<template>
+<h2>Card</h2>
+
+
+<div class="content">
+<div class="card">
+<slot name="default"></slot>
+  <p>name:{{ name }}</p>
+<slot name="info"></slot>
+</div>
+  
+</div>
+</template>
+
+<script>
+export default {
+  props: ['name']
+}
+</script>
+
+<style >
+.content{
+  margin: 10px;
+  background-color: blue;
+  padding: 30px;
+  width: 100%;
+  height: 300px;
+  border: 2px solid rgb(37, 214, 51);
+position: relative;
+}
+.card{
+   margin: 10px;
+  background-color: rgb(188, 188, 192);
+  width: 300px;
+  height: 300px;
+  border: 2px solid red;
+  float: left;
+
+}
+h1{
+  background-color: black;
+}
+</style>
